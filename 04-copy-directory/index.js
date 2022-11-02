128d892a7d5730182219646a956e39e118256b9a
KarinaBertosh <cherrycake246@gmail.com>
2022-11-02 17:52:33 +0300

    feat: make tasks 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,21 @@
-
 const fs = require('fs');
-const fsPromises = fs.promises;
+const path = require('path');
+const files = './04-copy-directory/files';
+const filesCopy = './04-copy-directory/files-copy';
+
+
+fs.mkdir(filesCopy, err => {
+  if (err) throw err;
+});
 
 
 
-function copyDir() {
-  fsPromises.mkdir('04-copy-directory/files-copy').then(function () {
-    console.log('Directory created successfully');
-  }).catch(function () {
-    console.log('failed to create directory');
+fs.readdir(files, (err, files) => {
+  files.forEach(files => {
+      console.log(files);
+      fs.copyFile(`./04-copy-directory/files/${files}`, `./04-copy-directory/files-copy/${files}`, (err) => {
+       if (err) throw err;
+       console.log('File Copy Successfully.'); 
+      });
+    })
   });
-}
-
-
-
-
-
-
-
